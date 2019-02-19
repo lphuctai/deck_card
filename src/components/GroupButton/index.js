2ac7945a12c8a270ce840cc2858892ab0c4df556
lphuctai <lphuctai@gmail.com>
2019-02-19 21:56:39 +0700

    Format code

--- src/components/GroupButton/index.js
+++ src/components/GroupButton/index.js
@@ -3,28 +3,28 @@ import {connect} from "react-redux";
 
 import * as deckAction from '../../actions/DeckAction'
 
-const GroupButton = (props) => {
+const GroupButton = ({deck_id, errorMessage, shuffleStatus, drawStatus, shuffleCard, revealCard, drawCard}) => {
     return (
         <div className="container">
             <div>
-                <p className="GroupButtonError">{props.errorMessage}</p>
+                <p className="GroupButtonError">{errorMessage}</p>
             </div>
             {
-                props.shuffleStatus === 'view' ?
+                shuffleStatus === 'view' ?
                     (<button className="btn btn-success" onClick={() => {
-                        props.shuffleCard()
+                        shuffleCard()
                     }}>Shuffler</button>) :
                     (<button className="btn btn-success" disabled>Shuffling</button>)
             }
             {
-                props.drawStatus === 'view' ?
+                drawStatus === 'view' ?
                     (<button className="btn btn-primary" onClick={() => {
-                        props.drawCard(props.deck_id)
+                        drawCard(deck_id)
                     }}>Draw</button>) :
                     (<button className="btn btn-primary" disabled>Drawing</button>)
             }
-            <button className="btn btn-danger" disabled={props.deck_id ? '' : 'disabled'} onClick={() => {
-                props.revealCard()
+            <button className="btn btn-danger" disabled={deck_id ? '' : 'disabled'} onClick={() => {
+                revealCard()
             }}>Reveal
             </button>
         </div>
